358f5b641ef227bf46332da963ff93e690cba68d
Lena Rowe <lena@lenarowe.com>
2017-12-04 18:49:14 +0100

    added listening model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204173858) do
+ActiveRecord::Schema.define(version: 20171204174703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "listenings", force: :cascade do |t|
+    t.integer  "user_id"
+    t.float    "ratio"
+    t.string   "type"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_listenings_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -36,4 +46,5 @@ ActiveRecord::Schema.define(version: 20171204173858) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "listenings", "users"
 end
