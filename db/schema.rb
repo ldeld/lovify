63307b60b908e55f897ce549c2e24f2c5375f9ee
Lorenzo Del Castillo <l.delcastillo.97@gmail.com>
2017-12-05 10:49:36 +0100

    working facebook sign-in

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204173858) do
+ActiveRecord::Schema.define(version: 20171204174332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,11 @@ ActiveRecord::Schema.define(version: 20171204173858) do
     t.string   "last_name"
     t.text     "description"
     t.string   "address"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "facebook_picture_url"
+    t.string   "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
