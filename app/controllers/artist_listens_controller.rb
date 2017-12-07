4181691ff1dc3ac5ff84ed53160ca96fbca4c1d5
Lorenzo Del Castillo <l.delcastillo.97@gmail.com>
2017-12-07 11:34:10 +0100

    added user match index

--- app/controllers/artist_listens_controller.rb
+++ app/controllers/artist_listens_controller.rb
@@ -1,8 +1,2 @@
 class ArtistListensController < ApplicationController
-  def self.create_artist_listens(list, user_id)
-    list.each_with_index do |artist, index|
-      saved_artist = Artist.find_by(spotify_id: artist.id)
-      ArtistListen.create(artist: saved_artist, user_id: user_id, rank: index + 1)
-    end
-  end
 end
