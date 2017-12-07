7bdf6bdcefd93223350a88497b67c62201b47132
Lena Rowe <lena@lenarowe.com>
2017-12-07 19:12:07 +0100

    yes link updates asker and no link sets hide to true but error if no match left

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,9 +1,9 @@
 class MatchesController < ApplicationController
-  before_action :set_match, only: [:show]
+  before_action :set_match, only: [:show, :ask_out, :not_interested, :update]
 
   def user_matches
     matches = Match.where(user_1: current_user) + Match.where(user_2: current_user)
-    matches.reject! { |m| m.score == 0 }
+    matches.reject! { |m| m.score == 0 || m.asker == current_user.id || m.hide == true}
     matches
   end
 
@@ -12,12 +12,35 @@ class MatchesController < ApplicationController
   end
 
   def show
+    @user = current_user
+  end
+
+  def ask_out
+    @match.asker = current_user.email
+    @match.save
+    redirect_to match_path(Match.all.where(hide: false).first.id)
+  end
+
+  def not_interested
+    @match.hide = true
+    @match.save
+    redirect_to match_path(Match.all.where(hide: false).first.id)
+  end
+
+  def update
+    @user = current_user
+    @match.update_attributes(match_params)
+    redirect_to match_path(Match.all.where(hide: false).first.id)
   end
 
   private
 
   def set_match
-    matches = Match.all.where(hide: false)
-    @match = matches.first
+    @match = user_matches.first
   end
+
+  def match_params
+    params.require(:match).permit(:hide, :asker)
+  end
+
 end
