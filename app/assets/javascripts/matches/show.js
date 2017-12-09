831a03f57b5ab94025058416fdb672a0504ad3fe
Natacha Ducre <natacha.ducre@gmail.com>
2017-12-09 17:32:00 +0100

    animation match show

--- app/assets/javascripts/matches/show.js
+++ app/assets/javascripts/matches/show.js
@@ -1,27 +1,46 @@
-var showFullList = document.getElementById("show-full-list");
-var showMoreInfo = document.getElementById("show-more-info");
+// var showFullList = document.getElementById("show-full-list");
+// var showMoreInfo = document.getElementById("show-more-info");
 var profileCard = document.getElementById("profile-card");
-var infoCard = document.getElementById("more-info")
+var card = document.querySelector(".card");
+// var infoCard = document.getElementById("more-info")
 
-if (showFullList) {
-  showFullList.addEventListener("click", function(event) {
-    event.preventDefault();
-    hiddenArtists = document.getElementById("artist-list")
-    hiddenArtists.classList.toggle("hidden");
-    hiddenArtists.classList.toggle("show-content");
+// if (showFullList) {
+//   showFullList.addEventListener("click", function(event) {
+//     event.preventDefault();
+//     hiddenArtists = document.getElementById("artist-list")
+//     hiddenArtists.classList.toggle("hidden");
+//     hiddenArtists.classList.toggle("show-content");
 
-    hiddenTracks = document.getElementById("track-list")
-    hiddenTracks.classList.toggle("hidden");
-    hiddenTracks.classList.toggle("show-content");
+//     hiddenTracks = document.getElementById("track-list")
+//     hiddenTracks.classList.toggle("hidden");
+//     hiddenTracks.classList.toggle("show-content");
 
 
-    showFullList.classList.add("hidden");
-  });
-};
+//     showFullList.classList.add("hidden");
+//   });
+// };
 
-showMoreInfo.addEventListener("click", function(event) {
-  event.preventDefault();
-  infoCard.classList.remove("hidden");
-  profileCard.classList.remove("col-xs-offset-3");
-  showMoreInfo.classList.add("hidden")
-});
+// showMoreInfo.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   infoCard.classList.remove("hidden");
+//   profileCard.classList.remove("col-xs-offset-3");
+//   showMoreInfo.classList.add("hidden")
+// });
+
+
+function triggerIn() {
+  card.setAttribute("class", "card animated slideOutLeft");
+  document.querySelector("#off").setAttribute("id", "on");
+
+  var x = document.createElement("div");
+  x.setAttribute("class", "fade");
+  document.querySelector(".wrapper").appendChild(x);
+
+  // profileCard.classList.remove( "col-xs-offset-3");
+
+}
+function triggerOut() {
+  document.querySelector("#on").setAttribute("id", "off");
+  var elem = document.querySelector(".fade");
+  elem.parentNode.removeChild(elem);
+}
