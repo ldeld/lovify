7c1af5568f5b8bd5385dd35c6864e1d7237be956
Lena Rowe <lena@lenarowe.com>
2017-12-11 18:54:41 +0100

    redirects to profile_path after edit

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,11 +2,15 @@ class RegistrationsController < Devise::RegistrationsController
 
  protected
 
- def update_resource(resource, params)
+  def update_resource(resource, params)
     resource.update_without_password(params)
   end
 
- def account_update_params
+  def after_update_path_for(resource)
+    '/next-match'
+  end
+
+  def account_update_params
     params.require(:user).permit(:first_name, :photo, :photo_cache, :description, :gender, :interested_in)
   end
 end
