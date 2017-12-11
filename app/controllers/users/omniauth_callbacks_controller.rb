1377c60bb8a78aecc782d16bcc75b8973ec62d18
Lena Rowe <lena@lenarowe.com>
2017-12-11 18:15:39 +0100

    start matching link in sidebar ok

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,26 +10,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       user.save
       if user.login_count == 1
         redirect_to edit_user_registration_path
-      else redirect_match
+      else redirect_to '/next-match'
       end
       set_flash_message(:notice, :success, kind: 'Spotify') if is_navigational_format?
     else
       session['devise.spotify_data'] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
     end
-  end
-
-  def redirect_match
-    if user_matches.empty?
-      redirect_to matches_no_matches_path
-    else
-      redirect_to match_path(Match.all.where(hide: false).first.id)
-    end
-  end
-
-  def user_matches
-    matches = Match.where(user_1: current_user) + Match.where(user_2: current_user)
-    matches.reject! { |m| m.score == 0 || m.asker == current_user.id || m.hide == true}
-    matches
   end
 end
