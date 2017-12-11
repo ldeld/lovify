a3c5afc036dbffde2aad6be2d508ac69a1996c67
Natacha Ducre <natacha.ducre@gmail.com>
2017-12-11 11:51:41 +0100

    change font color and cards color

--- app/assets/javascripts/matches/show.js
+++ app/assets/javascripts/matches/show.js
@@ -1,4 +1,4 @@
-// var showFullList = document.getElementById("show-full-list");
+var showFullList = document.getElementById("show-full-list");
 var showMoreInfo = document.getElementById("show-more-info");
 var profileCard = document.getElementById("profile-card");
 var card = document.querySelector(".card");
@@ -20,12 +20,7 @@ var card = document.querySelector(".card");
 //   });
 // };
 
-// showMoreInfo.addEventListener("click", function(event) {
-//   event.preventDefault();
-//   infoCard.classList.remove("hidden");
-//   profileCard.classList.remove("col-xs-offset-3");
-//   showMoreInfo.classList.add("hidden")
-// });
+
 
 
 function triggerIn() {
@@ -40,8 +35,13 @@ function triggerIn() {
   // profileCard.classList.remove( "col-xs-offset-3");
 
 }
-function triggerOut() {
-  document.querySelector("#on").setAttribute("id", "off");
-  var elem = document.querySelector(".fade");
-  elem.parentNode.removeChild(elem);
-}
+
+
+
+
+showMoreInfo.addEventListener("click", function(event) {
+  event.preventDefault();
+  infoCard.classList.remove("hidden");
+  profileCard.classList.remove("col-xs-offset-3");
+  showMoreInfo.classList.add("hidden")
+});
