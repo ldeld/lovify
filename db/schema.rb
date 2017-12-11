d81a29abafd72c5785e559a4c353071fb8bda98d
Lorenzo Del Castillo <l.delcastillo.97@gmail.com>
2017-12-11 16:21:00 +0100

    fixed schema issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171211142719) do
-
+ActiveRecord::Schema.define(version: 20171211140052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,27 +75,6 @@ ActiveRecord::Schema.define(version: 20171211142719) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "listen_in_commons", force: :cascade do |t|
-    t.integer  "listen_user_1_id"
-    t.integer  "listen_user_2_id"
-    t.integer  "match_id_id"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.index ["listen_user_1_id"], name: "index_listen_in_commons_on_listen_user_1_id", using: :btree
-    t.index ["listen_user_2_id"], name: "index_listen_in_commons_on_listen_user_2_id", using: :btree
-    t.index ["match_id_id"], name: "index_listen_in_commons_on_match_id_id", using: :btree
-  end
-
-  create_table "listenings", force: :cascade do |t|
-    t.integer  "user_id"
-    t.float    "ratio"
-    t.string   "type"
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_listenings_on_user_id", using: :btree
   end
 
   create_table "matches", force: :cascade do |t|
@@ -175,7 +153,6 @@ ActiveRecord::Schema.define(version: 20171211142719) do
     t.string   "gender"
     t.string   "photo"
     t.string   "spotify_auth"
-    t.string   "interested_in"
     t.string   "spotify_photo"
     t.integer  "login_count",            default: 0
     t.string   "interested_in"
