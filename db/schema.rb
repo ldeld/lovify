7be2d3d6aad90ab2570da77eb6112411f868842a
Lena Rowe <lena@lenarowe.com>
2017-12-11 14:09:08 +0100

    redirects user to edit only on sign up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171211105811) do
+ActiveRecord::Schema.define(version: 20171211125950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -148,6 +148,7 @@ ActiveRecord::Schema.define(version: 20171211105811) do
     t.string   "photo"
     t.string   "spotify_auth"
     t.string   "spotify_photo"
+    t.integer  "login_count",            default: 0
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
