6aaf9794974725db6ab19f054876318d43a3edad
Lorenzo Del Castillo <l.delcastillo.97@gmail.com>
2017-12-13 15:34:23 +0100

    changed index logic to conversation controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -3,7 +3,7 @@ class ConversationsController < ApplicationController
 
   def index
     @users = User.all
-    @conversations = Conversation.all
+    @conversations = Conversation.where(sender_id: current_user.id) + Conversation.where(recipient_id: current_user.id)
   end
 
   def create
