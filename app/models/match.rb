11925e459e5819d4d9ff65b82fb70e36e96029e5
Lena Rowe <lena@lenarowe.com>
2017-12-13 17:31:08 +0100

    better score et score max = 100

--- app/models/match.rb
+++ app/models/match.rb
@@ -18,7 +18,8 @@ class Match < ApplicationRecord
   def calculate_score
     match_user_artists
     match_user_tracks
-    self.score = (score * 1000).round / 100
+    calc = (score * 1000).round / 100
+    calc < 100 ? self.score = calc : self.score = 100
     self.save
   end
 
@@ -32,7 +33,7 @@ class Match < ApplicationRecord
         common.match = self
         common.save
         create_match_genre(artist_listen.artist)
-        self.score += (10 / (artist_listen.rank + user_2.artist_listens.where(artist: artist_listen.artist).first.rank)) * (Math.exp(- 0.8 - (artist_listen.artist.popularity / 100 )) + 1)
+        self.score += 1.1 * (10 / (artist_listen.rank + user_2.artist_listens.where(artist: artist_listen.artist).first.rank)) * (Math.exp(- 0.8 - (artist_listen.artist.popularity / 100 )) + 1)
       end
     end
   end
@@ -44,7 +45,7 @@ class Match < ApplicationRecord
         common.track_listen_user_2 = user_2.track_listens.where(track: track_listen.track).first
         common.match = self
         common.save
-        self.score += 0.8 * (10 / (track_listen.rank + user_2.track_listens.where(track: track_listen.track).first.rank)) * (Math.exp(- 0.8 - (track_listen.track.popularity / 100 )) + 1)
+        self.score += 0.5 * (10 / (track_listen.rank + user_2.track_listens.where(track: track_listen.track).first.rank)) * (Math.exp(- 0.8 - (track_listen.track.popularity / 100 )) + 1)
 
       end
     end
