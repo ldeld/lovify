5b4918ef0900ce1325c419fa6e74f58999fdc3fe
Lorenzo Del Castillo <l.delcastillo.97@gmail.com>
2017-12-13 16:10:25 +0100

    send message btn on date view

--- app/controllers/rdvs_controller.rb
+++ app/controllers/rdvs_controller.rb
@@ -20,7 +20,8 @@ class RdvsController < ApplicationController
   def accept
     rdv = Rdv.find(params[:id])
     rdv.update(accepted: true)
-    Conversation.create!(sender_id: asker.id, recipient_id: receiver.id, rdv_id: rdv)
+    Conversation.create!(sender_id: rdv.asker.id, recipient_id: rdv.receiver.id, rdv_id: rdv.id)
+    binding.pry
     redirect_to rdvs_path
   end
 
