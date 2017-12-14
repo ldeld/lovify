e865c8dd1a45ab5b6b9df921e35bf5bb15484552
Lorenzo Del Castillo <l.delcastillo.97@gmail.com>
2017-12-14 14:39:15 +0100

    bar map for every bar

---
+++ app/assets/javascripts/show_map.js
@@ -0,0 +1,11 @@
+function drawMap(selectorDom, markerJson) {
+  console.log(selectorDom);
+  console.log(markerJson);
+  var handler = Gmaps.build('Google');
+    handler.buildMap({ internal: { id: selectorDom } }, function() {
+      markers = handler.addMarkers(markerJson);
+      handler.bounds.extendWith(markers);
+      handler.fitMapToBounds();
+      handler.getMap().setZoom(14);
+    });
+  }
