48dc045de094dd562ad094183ede3a51eb350d7d
Lorenzo Del Castillo <l.delcastillo.97@gmail.com>
2017-12-14 13:48:46 +0100

    map has marker now

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,13 +1,6 @@
 class BarsController < ApplicationController
   before_action :find_bar
 
-  def show
-    @markers = Gmaps4rails.build_markers(@bar) do |bar, marker|
-      marker.lat bar.latitude
-      marker.lng bar.longitude
-      # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
-    end
-  end
 
   private
 
