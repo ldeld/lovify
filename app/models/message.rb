f213887c482ae1bf8de1a1621cb46d4f0b49fb8d
Lorenzo Del Castillo <l.delcastillo.97@gmail.com>
2018-02-22 10:37:38 +0100

    cleaned up js files

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ActiveRecord::Base
  belongs_to :conversation
  belongs_to :user
  validates_presence_of :body, :conversation_id, :user_id
+
  def message_time
   created_at.strftime("%l:%M %p")
  end
